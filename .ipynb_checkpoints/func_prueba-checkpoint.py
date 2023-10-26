b3848f900ea8028d8f9a22a7056870ebc98877be
Rodrigo Fernandez Campos <rodrifer10.c@gmail.com>
2023-10-26 17:50:04 +0200

    actualizacion del repo al 26-10 con Class4

---
+++ .ipynb_checkpoints/func_prueba-checkpoint.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+def dame_variables_categoricas(dataset=None):
+    '''
+    ----------------------------------------------------------------------------------------------------------
+    Función dame_variables_categoricas:
+    ----------------------------------------------------------------------------------------------------------
+        -Descripción: Función que recibe un dataset y devuelve una lista con los nombres de las 
+        variables categóricas
+        -Inputs: 
+            -- dataset: Pandas dataframe que contiene los datos
+        -Return:
+            -- lista_variables_categoricas: lista con los nombres de las variables categóricas del
+            dataset de entrada con menos de 100 valores diferentes
+            -- 1: la ejecución es incorrecta
+    '''
+    if dataset is None:
+        print(u'\nFaltan argumentos por pasar a la función')
+        return 1
+    lista_variables_categoricas = []
+    other = []
+    for i in dataset.columns:
+        if (dataset[i].dtype!=float) & (dataset[i].dtype!=int):
+            unicos = int(len(np.unique(dataset[i].dropna(axis=0, how='all'))))
+            if unicos < 100:
+                lista_variables_categoricas.append(i)
+            else:
+                other.append(i)
+
+    return lista_variables_categoricas, other
